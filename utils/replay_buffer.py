ea6ff9a003f8663fb47e7e39c97da0f3f90a45ea
Joan Lleo <joanlleo@me.com>
2025-09-27 14:22:56 +0200

    Replay buffer class implemented

---
+++ utils/replay_buffer.py
@@ -0,0 +1,18 @@
+import random
+from collections import deque, namedtuple
+
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'done'))
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = deque(maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
